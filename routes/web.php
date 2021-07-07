8856a7c7bc01316b3821419fc3bd5e10e91e7d34
Jeshica216 <Jessicaangelinagatur21@gmail.com>
2021-07-07 16:54:48 +0700

    telah mengerjakan

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('pesan/{id}', 'PesanController@index');
+Route::post('pesan/{id}', 'PesanController@pesan');
+Route::get('check-out', 'PesanController@check_out');
+Route::delete('check-out/{id}', 'PesanController@delete');
+
+Route::get('konfirmasi-check-out', 'PesanController@konfirmasi');
+
+Route::get('profile', 'ProfileController@index');
+Route::post('profile', 'ProfileController@update');
+
+Route::get('history', 'HistoryController@index');
+Route::get('history/{id}', 'HistoryController@detail');
